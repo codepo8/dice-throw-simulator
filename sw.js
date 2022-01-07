3cada9f9c40d3090ee56d638e2cfbf64ee2d42f2
Christian Heilmann <chris.heilmann@gmail.com>
2022-01-07 18:52:02 +0100

    Adding manifest and service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'version_01'
+var URLS = [           
+  '/',                 
+  '/index.html'
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (request) {
+      return request || fetch(event.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key, i) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+})
